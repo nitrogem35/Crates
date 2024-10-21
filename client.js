08b75ee4813e8a1358e9ad08ce032ef5b3b5dd40
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-10-20 22:25:09 -0500

    changed index.js to client.js

---
+++ client.js
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+let cratesEngine;
+if (fs.existsSync('./lib/crates-engine.node')) {
+    cratesEngine = await import('./lib/crates-engine.node');
+} else {
+    console.log("Fallback to JS engine");
+    cratesEngine = await import('./lib/crates-engine.js');
+}
+const { util, connectToServer } = cratesEngine;
+//General configuration
+const APIBase = "index.gats.io/api";
+util.setAPIBase(APIBase);
+
+//TODO: Remove this when gats has proper ssl set up.
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+//When launched, we should just connect to the best server
+await util.grabServers();
+await util.testAllPing();
+let world = await connectToServer(util.serverList.sort((a, b) => a.ping - b.ping)[0].url);
+
