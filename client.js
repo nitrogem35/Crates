002c960d3a66be4aae3e43f79d751a232a9874eb
nitrogem35 <69062894+nitrogem35@users.noreply.github.com>
2024-10-21 00:02:50 -0500

    feat: can now join the game

--- client.js
+++ client.js
@@ -6,13 +6,28 @@ if (fs.existsSync('./lib/crates-engine.node')) {
     console.log("Fallback to JS engine");
     cratesEngine = await import('./lib/crates-engine.js');
 }
-const { util, connectToServer } = cratesEngine;
+const { util, connectToServer, Util } = cratesEngine;
 //General configuration
 const APIBase = "index.gats.io/api";
 util.setAPIBase(APIBase);
+
+//Setup game UI stuff
+const ingame = false;
+const loadout = [Util.guns.indexOf("sniper"), Util.armors.indexOf("no_armor"), Util.colors.indexOf("green")];
 
 //When launched, we should just connect to the best server
 await util.grabServers();
 await util.testAllPing();
 let world = await connectToServer(util.serverList.sort((a, b) => a.ping - b.ping)[0].url);
+let { controller } = world;
 
+world.emitter.on("socketOpened", () => {
+    console.log("Spawning in")
+    controller.joinGame(loadout[0], loadout[1], loadout[2]);
+});
+
+world.emitter.on("socketClosed", () => {
+    if (ingame) {
+        ingame = false;
+    }
+});
